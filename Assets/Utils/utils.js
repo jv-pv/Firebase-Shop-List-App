06f47fd09b4e58b144734529c5b7e9555e3eb90c
John Pieri <jvpieri@gmail.com>
2023-06-21 17:44:45 -0400

    Commit

--- Assets/Utils/utils.js
+++ Assets/Utils/utils.js
@@ -1,14 +1,24 @@
 let count = 0
+const inputFieldItem = document.getElementById('input-field-item')
+const inputFieldQuantity = document.getElementById('input-field-quantity')
+const shopppingListEl = document.getElementById('shopping-list')
 
 function countUp() {
     count++
     return count
 }
 
-function appendListItemsToHtml(itemValue, quantityValue) {
-    const shopppingListEl = document.getElementById('shopping-list')
-    shopppingListEl.innerHTML += `<li><span class='list-item'>${itemValue}</span>: x ${quantityValue}`
+function appendListItemsToHtml(count, itemValue, quantityValue) {
+    shopppingListEl.innerHTML += `<li> Type: <i>${itemValue}</i> <span class="list-item">Quantity: <i>${quantityValue}<i></span></li>`
 }
 
+function appendDBValuesToHtml(items) {
+    shopppingListEl.innerHTML += `<li>${items}</li>`
+}
 
-export { countUp, appendListItemsToHtml }
+function clearListItem() {
+    inputFieldItem.value = ""
+    inputFieldQuantity.value = ""
+}
+
+export { countUp, appendListItemsToHtml, appendDBValuesToHtml, clearListItem }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-import {countUp, appendListItemsToHtml} from './Assets/Utils/utils.js'
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { countUp, appendListItemsToHtml, appendDBValuesToHtml, clearListItem } from './Assets/Utils/utils.js'
 
 const appSettings = {
     databaseURL: "https://playground-4d045-default-rtdb.firebaseio.com/"
@@ -10,19 +10,28 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "Shopping-List")
 
-const addButtonEl = document.getElementById("add-button")
+const addButton = document.getElementById("add-button")
 
-addButtonEl.addEventListener("click", function(e) {
+addButton.addEventListener("click", function(e) {
     e.preventDefault()
     let count = countUp()
-    let inputFieldItemValue = document.getElementById('input-field-item').value
-    let inputFieldQuantityValue = document.getElementById('input-field-quantity').value
-
+    const inputFieldItemValue = document.getElementById('input-field-item').value
+    const inputFieldQuantityValue = document.getElementById('input-field-quantity').value
     if (inputFieldItemValue === "" || inputFieldQuantityValue === "") {
         return 
     } else {
-        appendListItemsToHtml(inputFieldItemValue, inputFieldQuantityValue)
-        let listItem = ` item ${count}) ${inputFieldItemValue}, quantity: ${inputFieldQuantityValue} `
+        clearListItem()
+        appendListItemsToHtml(count, inputFieldItemValue, inputFieldQuantityValue)
+        let listItem = `Type: ${inputFieldItemValue}, Quantity: ${inputFieldQuantityValue} `
         push(shoppingListInDB, listItem)
     }
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+    let shoppingListItems = Object.values(snapshot.val())
+    clearListItem()
+    for (let items of shoppingListItems) {
+        appendDBValuesToHtml(items)
+    }
+    
 })

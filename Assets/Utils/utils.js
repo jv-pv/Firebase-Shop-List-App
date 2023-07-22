b507275dacf6099185fd01457338a4f154a6b51d
John Pieri <jvpieri@gmail.com>
2023-07-21 21:47:27 -0400

    Removed unused functions

--- Assets/Utils/utils.js
+++ Assets/Utils/utils.js
@@ -1,15 +1,9 @@
-let count = 0
 const inputFieldItem = document.getElementById('input-field-item')
 const inputFieldQuantity = document.getElementById('input-field-quantity')
 const shopppingListEl = document.getElementById('shopping-list')
 
-function countUp() {
-    count++
-    return count
-}
-
-function setListItemsToHtml(itemValue, quantityValue) {
-    shopppingListEl.innerHTML += `${itemValue} x ${quantityValue} `
+function setListItemsToHtml(selectFromValue ,itemValue, quantityValue) {
+    shopppingListEl.innerHTML += `${selectFromValue}: ${itemValue} x ${quantityValue} `
 }
 
 function clear() {
@@ -18,4 +12,4 @@ function clear() {
     shopppingListEl.innerHTML = ""
 }
 
-export { countUp, setListItemsToHtml, clear }
+export { setListItemsToHtml, clear }
